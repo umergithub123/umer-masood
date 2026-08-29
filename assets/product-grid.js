7665d7024302b7fe71064a92c6ed061091eafea0
umergithub123 <umerchaudhry.089@gmail.com>
2026-08-29 20:24:44 +0500

    customization-settings

--- assets/product-grid.js
+++ assets/product-grid.js
@@ -9,12 +9,20 @@ document.addEventListener('DOMContentLoaded', ()=> {
     })
   })
 
+  // close event 
   let quick_view_close = document.querySelectorAll('.quick-view__close');
   quick_view_close.forEach((close_button)=> {
     close_button.addEventListener('click',(e)=> {
       let currentElement = e.currentTarget;
       let currentQuickview = currentElement.closest('.product-grid__grid-image').querySelector('quick-view-template');
       currentQuickview.classList.remove('active');
+    })
+  })
+
+  let quick_view_template = document.querySelectorAll('quick-view-template');
+  quick_view_template.forEach((template)=>{
+    template.addEventListener('click',(e)=> {
+      if (e.currentTarget.closest == 'quick-view__holder' && e.currentTarget != '')
     })
   })
 })
